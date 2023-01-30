a948e18740d2eb08b9958f85f0c962885df007f5
luckyman1004 <luckysuperdev@gmail.com>
2023-01-30 18:34:53 +0530

    Getting started with Ruby

---
+++ first.rb
@@ -0,0 +1,13 @@
+# Types of Print in Ruby
+puts "Hello World!"
+print "Hello World!"
+p "Hello World!"
+
+# Parametric Function
+def sayHello(name)
+    puts "GG " + name + "!"
+end
+
+# Variable
+name = "Boys"
+sayHello(name)

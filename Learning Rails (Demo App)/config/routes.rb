64e8c826e41c853e4f095fd5ef485f0c77047841
luckyman1004 <luckysuperdev@gmail.com>
2023-02-10 18:39:12 +0530

    Started with outline CRUD ops, db migration and scaffold generators

--- Learning Rails (Demo App)/config/routes.rb
+++ Learning Rails (Demo App)/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   resources :users
   root 'pages#home'
   get 'about', to: 'pages#about'
--- Learning Rails (Demo App)/db/schema.rb
+++ Learning Rails (Demo App)/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_065945) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_124209) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"

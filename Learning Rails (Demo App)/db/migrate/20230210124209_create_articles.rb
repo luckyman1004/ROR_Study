98ab5d5e704e6b9dbc62dd824e1360d8ceeb3e70
luckyman1004 <luckysuperdev@gmail.com>
2023-02-12 21:04:48 +0530

    Working with Rails db-migrations

--- Learning Rails (Demo App)/db/migrate/20230210124209_create_articles.rb
+++ Learning Rails (Demo App)/db/migrate/20230210124209_create_articles.rb
@@ -3,8 +3,6 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       t.string :title
       t.text :description
-
-      t.timestamps
     end
   end
 end
--- Learning Rails (Demo App)/db/schema.rb
+++ Learning Rails (Demo App)/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_124209) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_152850) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|

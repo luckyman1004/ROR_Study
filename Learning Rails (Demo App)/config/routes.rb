bdead84c658b383b75087cce316b6691b6e34ed5
luckyman1004 <luckysuperdev@gmail.com>
2023-02-13 10:37:17 +0530

    Using the Show Article Feature

--- Learning Rails (Demo App)/config/routes.rb
+++ Learning Rails (Demo App)/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, only: [:show]          # Required to get all the show routes for articles
   resources :users
   root 'pages#home'
   get 'about', to: 'pages#about'

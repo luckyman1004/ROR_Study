fda7ffc2d08cbe1e19ec4c811d16736803b74bde
luckyman1004 <luckysuperdev@gmail.com>
2023-02-13 16:44:55 +0530

    Created the Articles listing page (:index)

--- Learning Rails (Demo App)/config/routes.rb
+++ Learning Rails (Demo App)/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]          # Required to get all the show routes for articles
+  resources :articles, only: [:show, :index]          # Required to get all the show and index routes for articles
   resources :users
   root 'pages#home'
   get 'about', to: 'pages#about'

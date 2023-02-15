4791b55907660f51516a13a02eb904ee8b539099
luckyman1004 <luckysuperdev@gmail.com>
2023-02-15 16:29:02 +0530

    Implemented Edit Action for Articles

--- Learning Rails (Demo App)/app/controllers/articles_controller.rb
+++ Learning Rails (Demo App)/app/controllers/articles_controller.rb
@@ -11,8 +11,24 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:description, :title))
+
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
--- Learning Rails (Demo App)/config/routes.rb
+++ Learning Rails (Demo App)/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end

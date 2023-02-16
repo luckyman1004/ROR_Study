2142aaffd63c485a151f444066a0de44f5cc4ff9
luckyman1004 <luckysuperdev@gmail.com>
2023-02-16 16:34:22 +0530

    Removed User Sacffold and created users table and user model manually

--- Learning Rails (Demo App)/config/routes.rb
+++ Learning Rails (Demo App)/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles   # To include all routes of articles
--- Learning Rails (Demo App)/db/schema.rb
+++ Learning Rails (Demo App)/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_112237) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_105503) do
   create_table "articles", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", null: false
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_112237) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
